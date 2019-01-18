2f9c245b818075c55092959698cc21890be9f871
Ray Gonzales <raygon8712@gmail.com>
2019-01-18 11:32:11 -0700

    added css module

--- front-end-admin-producer/src/Producer/components/AddItem/AddMeatForm/AddMeatForm.js
+++ front-end-admin-producer/src/Producer/components/AddItem/AddMeatForm/AddMeatForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './AddMeatForm.css';
+import './AddMeatForm.module.css';
 
 class ProducerForm extends Component {  
     state = {
--- front-end-admin-producer/src/Producer/containers/Layout/Layout.js
+++ front-end-admin-producer/src/Producer/containers/Layout/Layout.js
@@ -1,9 +1,5 @@
 import React, {Fragment} from 'react';
-import SignIn from '../../../SharedComponents/SignIn';
 import Summary from '../../components/Summary/Summary';
-import ContactUs from '../../components/ContactUs/ContactUs';
-import AddItem from '../../components/AddItem/AddItem';
-import ProductDetail from '../../components/ProductDetail/ProductDetail';
 
 const layout = () => {
    return (
