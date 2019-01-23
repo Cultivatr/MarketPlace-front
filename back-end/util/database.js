7de34ca4b7f864b6a43cd836aaa6401af39494e7
Jeff Kwok <jkwok97@gmail.com>
2019-01-23 10:36:52 -0700

    revising username in database.js

--- back-end/util/database.js
+++ back-end/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('cultivatr', 'postgres', 'Password', {
+const sequelize = new Sequelize('cultivatr', 'evolveu', 'Password', {
    dialect: 'postgres',
    host: 'localhost'
 });
