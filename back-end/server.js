cfbd72284d86ccdc2f81c233beed4d6157f380c0
evolveu <lemi.m@outlook.com>
2019-01-24 15:56:33 -0700

    building a test api route

--- back-end/server.js
+++ back-end/server.js
@@ -11,23 +11,40 @@ const R_facility = require('./models/r_facility');
 
 const app = express();
 
+app.get('/', function(req, res, next) {
+	Users.findAll()
+		.then((lemi) =>
+			res.json({
+				error: false,
+				data: lemi
+			})
+		)
+		.catch((error) =>
+			res.json({
+				error: true,
+				data: [],
+				error: error
+			})
+		);
+});
+
 // Relationships
 // Product.belongsTo(User);
 // User.hasMany(Product);
-Offered_item.belongsTo(Users, {foreignKey: "user_id"});
-Offered_item.belongsTo(R_product, {foreignKey: "product_id"});
+Offered_item.belongsTo(Users, { foreignKey: 'user_id' });
+Offered_item.belongsTo(R_product, { foreignKey: 'product_id' });
 // Offered_item.belongsTo(Status);
-Status_tracker.belongsTo(Offered_item, {foreignKey: "offer_id"});
-Status_tracker.belongsTo(R_status, {foreignKey: "status_id"});
+Status_tracker.belongsTo(Offered_item, { foreignKey: 'offer_id' });
+Status_tracker.belongsTo(R_status, { foreignKey: 'status_id' });
 
 // ****** this method will drop existing tables and re-create tables, you will lose your data
 // ****** comment this out when going into production
 
-sequelize.sync({force:true}); 
+// sequelize.sync({ force: true });
 
 // ******************************************************************************************
 
 const port = 8080;
 app.listen(port, () => {
-   console.log(`Server running on port ${port}`)
-}); 
+	console.log(`Server running on port ${port}`);
+});
