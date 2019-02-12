1a3856dc1d6b5b6df5666d20a911243ccaef7c65
Jeff Kwok <jkwok97@gmail.com>
2019-02-12 11:15:50 -0700

    start back-end in python

---
+++ back-end/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from datetime import datetime
+import server_func
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret'
+
+@app.route("/all_items")
+def all_items():
+
+    items = server_func.get_all_offered_items_by_user_id(1)
+    return jsonify(items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ back-end/server_func.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+class Items:
+    def __init__(self, id):
+        self.id = id
+
+def get_all_offered_items_by_user_id(self,id):
+    conn = psycopg2.connect("dbname=cultivatr")
+    cur = conn.cursor()
+
+    cur.execute(f'SELECT product_id FROM offered_item where user_id = {id};')
+
+    offered_item_array = cur.fetchall()
+    
+    offered_item_obj_array = []
+    for items in offered_item_array:
+        newItem = Items(items)
+        offered_item_obj_array.append(newItem.__dict__)
+
+    cur.close()
+    conn.close()
+    
+    return offered_item_obj_array
