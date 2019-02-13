63e22eec2675a6af644c1ce89e84c3e68e0ebf1a
Ray Gonzales <raygon8712@gmail.com>
2019-02-13 11:56:26 -0700

    added border to form

--- front-end-admin-producer/src/Producer/components/AddItem/AddMeatForm/AddMeatForm.js
+++ front-end-admin-producer/src/Producer/components/AddItem/AddMeatForm/AddMeatForm.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './AddMeatForm.module.css';
+import React, { Component, Fragment } from 'react';
+import styles from './AddMeatForm.module.css';
 import Button from '../../../../SharedComponents/UI/Button';
 
 class LivestockForm extends Component {  
@@ -36,105 +36,110 @@ class LivestockForm extends Component {
 
     render() {
         return (
-        <div className='ui grid container'>
-            <form onSubmit={this.onSubmit} className='ui row form'>
-                <div className='eight wide column'>
-                    <div className='field'>
-                        <label>Type</label>
-                        <select onChange={this.onChange} name="type" multiple="" className="ui fluid dropdown">
-                            <option value="">Please choose an option</option>
-                            <option value="pork">Pork</option>
-                            <option value="chicken">Chicken</option>
-                            <option value="lamb">Lamb</option>
-                            <option value="goat">Goat</option>
-                            <option value="custom">Custom</option>
-                        </select>
-                    </div>
-                    <div className='field'>
-                        <label>Breed</label>
-                        <select onChange={this.onChange} name="breed" multiple="" className="ui fluid dropdown">
-                            <option value="">Please choose an option</option>
-                            <option value="angus">Angus</option>
-                            <option value="birkshire">Birkshire</option>
-                            <option value="custom">Custom</option>
-                        </select>
-                    </div>
-                    <div className="field">
-                        <label>Birthdate</label>
-                        <input onChange={this.onChange} type="text" name="birthdate"/>
-                    </div>
-                    <div className="field">
-                        <label>Registration Number</label>
-                        <input onChange={this.onChange} type="text" name="regNumber"/>
-                    </div>
-                    <div className="field">
-                        <label>RFID Tag</label>
-                        <input onChange={this.onChange} type="text" name="rfid"/>
-                    </div>
-                    <div className="field">
-                        <label>Est. Starting Weight</label>
-                        <input onChange={this.onChange} type="text" name="estStartingWeight"/>
-                    </div>
-                    <div className="field">
-                        <label>Hanging Weight</label>
-                        <input onChange={this.onChange} type="text" name="hangingWeight"/>
-                    </div>
-                    <div className="field">
-                        <label>Chargebacks</label>
-                        <input onChange={this.onChange} type="text" name="chargebacks"/>
+            <Fragment>
+                <h2>Add Livestock</h2>
+                <div className={styles.wrapper}>
+                    <div className='ui grid container'>
+                        <form onSubmit={this.onSubmit} className='ui row form'>
+                            <div className='eight wide column'>
+                                <div className='field'>
+                                    <label>Type</label>
+                                    <select onChange={this.onChange} name="type" multiple="" className="ui fluid dropdown">
+                                        <option value="">Please choose an option</option>
+                                        <option value="pork">Pork</option>
+                                        <option value="chicken">Chicken</option>
+                                        <option value="lamb">Lamb</option>
+                                        <option value="goat">Goat</option>
+                                        <option value="custom">Custom</option>
+                                    </select>
+                                </div>
+                                <div className='field'>
+                                    <label>Breed</label>
+                                    <select onChange={this.onChange} name="breed" multiple="" className="ui fluid dropdown">
+                                        <option value="">Please choose an option</option>
+                                        <option value="angus">Angus</option>
+                                        <option value="birkshire">Birkshire</option>
+                                        <option value="custom">Custom</option>
+                                    </select>
+                                </div>
+                                <div className="field">
+                                    <label>Birthdate</label>
+                                    <input onChange={this.onChange} type="text" name="birthdate"/>
+                                </div>
+                                <div className="field">
+                                    <label>Registration Number</label>
+                                    <input onChange={this.onChange} type="text" name="regNumber"/>
+                                </div>
+                                <div className="field">
+                                    <label>RFID Tag</label>
+                                    <input onChange={this.onChange} type="text" name="rfid"/>
+                                </div>
+                                <div className="field">
+                                    <label>Est. Starting Weight</label>
+                                    <input onChange={this.onChange} type="text" name="estStartingWeight"/>
+                                </div>
+                                <div className="field">
+                                    <label>Hanging Weight</label>
+                                    <input onChange={this.onChange} type="text" name="hangingWeight"/>
+                                </div>
+                                <div className="field">
+                                    <label>Chargebacks</label>
+                                    <input onChange={this.onChange} type="text" name="chargebacks"/>
+                                </div>
+                            </div>
+                            <div className='eight wide column'>
+                                <div className="field">
+                                    <label>Date on Feed</label>
+                                    <input onChange={this.onChange} type="text" name="dateOnFeed"/>
+                                </div>
+                                <div className='field'>
+                                    <label>Feed Method</label>
+                                    <select onChange={this.onChange} name="feedMethod" multiple="" className="ui fluid dropdown">
+                                        <option value="">Please choose an option</option>
+                                        <option value="grass">Grass</option>
+                                        <option value="grassBarley">Grass and Barley Finished</option>
+                                        <option value="grassGrain">Grass and Grain Finished</option>
+                                        <option value="freeRange">Free Range</option>
+                                        <option value="custom">Custom</option>
+                                    </select>
+                                </div>
+                                <div className='field'>
+                                    <label>Type of Pasture</label>
+                                    <select onChange={this.onChange} name="typeOfPasture" multiple="" className="ui fluid dropdown">
+                                        <option value="">Please choose an option</option>
+                                        <option value="test1">test</option>
+                                        <option value="test2">test</option>
+                                        <option value="test3">test</option>
+                                        <option value="custom">Custom</option>
+                                    </select>
+                                </div>
+                                <div className='field'>
+                                    <label>Type of Feed</label>
+                                    <select onChange={this.onChange} name="typeOfFeed" multiple="" className="ui fluid dropdown">
+                                        <option value="">Please choose an option</option>
+                                        <option value="grain">Grain</option>
+                                        <option value="barley">Barley</option>
+                                        <option value="custom">Custom</option>
+                                    </select>
+                                </div>
+                                <div className="field">
+                                    <label>Est. Completion Date</label>
+                                    <input onChange={this.onChange} type="text" name="estCompletionDate"/>
+                                </div>
+                                <div className="field">
+                                    <label>Est. Finished Weight</label>
+                                    <input onChange={this.onChange} type="text" name="estFinishedWeight"/>
+                                </div>
+                                <div className="field">
+                                    <label>Est. Final Price to be Paid</label>
+                                    <input onChange={this.onChange} type="number" name="estFinalPrice"/>
+                                </div>
+                            </div>
+                            <Button>Add</Button>
+                        </form>
                     </div>
                 </div>
-                <div className='eight wide column'>
-                    <div className="field">
-                        <label>Date on Feed</label>
-                        <input onChange={this.onChange} type="text" name="dateOnFeed"/>
-                    </div>
-                    <div className='field'>
-                        <label>Feed Method</label>
-                        <select onChange={this.onChange} name="feedMethod" multiple="" className="ui fluid dropdown">
-                            <option value="">Please choose an option</option>
-                            <option value="grass">Grass</option>
-                            <option value="grassBarley">Grass and Barley Finished</option>
-                            <option value="grassGrain">Grass and Grain Finished</option>
-                            <option value="freeRange">Free Range</option>
-                            <option value="custom">Custom</option>
-                        </select>
-                    </div>
-                    <div className='field'>
-                        <label>Type of Pasture</label>
-                        <select onChange={this.onChange} name="typeOfPasture" multiple="" className="ui fluid dropdown">
-                            <option value="">Please choose an option</option>
-                            <option value="test1">test</option>
-                            <option value="test2">test</option>
-                            <option value="test3">test</option>
-                            <option value="custom">Custom</option>
-                        </select>
-                    </div>
-                    <div className='field'>
-                        <label>Type of Feed</label>
-                        <select onChange={this.onChange} name="typeOfFeed" multiple="" className="ui fluid dropdown">
-                            <option value="">Please choose an option</option>
-                            <option value="grain">Grain</option>
-                            <option value="barley">Barley</option>
-                            <option value="custom">Custom</option>
-                        </select>
-                    </div>
-                    <div className="field">
-                        <label>Est. Completion Date</label>
-                        <input onChange={this.onChange} type="text" name="estCompletionDate"/>
-                    </div>
-                    <div className="field">
-                        <label>Est. Finished Weight</label>
-                        <input onChange={this.onChange} type="text" name="estFinishedWeight"/>
-                    </div>
-                    <div className="field">
-                        <label>Est. Final Price to be Paid</label>
-                        <input onChange={this.onChange} type="number" name="estFinalPrice"/>
-                    </div>
-                </div>
-                <Button>Add</Button>
-            </form>
-        </div>
+            </Fragment>
         )
     }
 }
--- front-end-admin-producer/src/Producer/components/AddItem/AddProduceForm/AddProduceForm.js
+++ front-end-admin-producer/src/Producer/components/AddItem/AddProduceForm/AddProduceForm.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './AddProduceForm.module.css';
+import Button from '../../../../SharedComponents/UI/Button';
+import styles from './AddProduceForm.module.css';
 
 class ProduceForm extends Component {  
     state = {
@@ -33,71 +35,76 @@ class ProduceForm extends Component {
 
     render() {
         return (
-        <div className='ui grid container'>
-            <form onSubmit={this.onSubmit} className='ui row form'>
-                <div className='eight wide column'>
-                    <div className="field">
-                        <label>Date Planted</label>
-                        <input onChange={this.onChange} type="text" name="datePlanted"/>
-                    </div>
-                    <div className="field">
-                        <label>Seed Type</label>
-                        <input onChange={this.onChange} type="text" name="seedType"/>
-                    </div>
-                    <div className='field'>
-                        <label>Modified Seed</label>
-                        <select onChange={this.onChange} name="modifiedSeed" multiple="" className="ui fluid dropdown">
-                            <option value="">Please choose an option</option>
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                    </div>
-                    <div className='field'>
-                        <label>Hierloom</label>
-                        <select onChange={this.onChange} name="hierloom" multiple="" className="ui fluid dropdown">
-                            <option value="">Please choose an option</option>
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                    </div>
-                    <div className="field">
-                        <label>Fertilizer Type Used</label>
-                        <input onChange={this.onChange} type="text" name="fertilizerTypeUsed"/>
-                    </div>
-                    <div className="field">
-                        <label>Pesticide Type Used</label>
-                        <input onChange={this.onChange} type="text" name="pesticideTypeUsed"/>
+            <Fragment>
+            <h2>Add Produce</h2>
+                <div className={styles.wrapper}>
+                    <div className='ui grid container'>
+                        <form onSubmit={this.onSubmit} className='ui row form'>
+                            <div className='eight wide column'>
+                                <div className="field">
+                                    <label>Date Planted</label>
+                                    <input onChange={this.onChange} type="text" name="datePlanted"/>
+                                </div>
+                                <div className="field">
+                                    <label>Seed Type</label>
+                                    <input onChange={this.onChange} type="text" name="seedType"/>
+                                </div>
+                                <div className='field'>
+                                    <label>Modified Seed</label>
+                                    <select onChange={this.onChange} name="modifiedSeed" multiple="" className="ui fluid dropdown">
+                                        <option value="">Please choose an option</option>
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </select>
+                                </div>
+                                <div className='field'>
+                                    <label>Hierloom</label>
+                                    <select onChange={this.onChange} name="hierloom" multiple="" className="ui fluid dropdown">
+                                        <option value="">Please choose an option</option>
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </select>
+                                </div>
+                                <div className="field">
+                                    <label>Fertilizer Type Used</label>
+                                    <input onChange={this.onChange} type="text" name="fertilizerTypeUsed"/>
+                                </div>
+                                <div className="field">
+                                    <label>Pesticide Type Used</label>
+                                    <input onChange={this.onChange} type="text" name="pesticideTypeUsed"/>
+                                </div>
+                            </div>
+                            <div className='eight wide column'>
+                                <div className="field">
+                                    <label>Estimated Finished Qty</label>
+                                    <input onChange={this.onChange} type="text" name="estimatedFinishedQty"/>
+                                </div>
+                                <div className="field">
+                                    <label>Est Price</label>
+                                    <input onChange={this.onChange} type="text" name="estPrice"/>
+                                </div>
+                                <div className="field">
+                                    <label>Qty Accepted for Listing</label>
+                                    <input onChange={this.onChange} type="text" name="qtyAcceptedForListing"/>
+                                </div>
+                                <div className="field">
+                                    <label>Qty Accepted at Delivery</label>
+                                    <input onChange={this.onChange} type="text" name="qtyAcceptedAtDelivery"/>
+                                </div>
+                                <div className="field">
+                                    <label>Chargebacks</label>
+                                    <input onChange={this.onChange} type="text" name="chargebacks"/>
+                                </div>
+                                <div className="field">
+                                    <label>Final Price Paid</label>
+                                    <input onChange={this.onChange} type="number" name="finalPricePaid"/>
+                                </div>
+                            </div>
+                            <Button>Add</Button>
+                        </form>
                     </div>
                 </div>
-                <div className='eight wide column'>
-                    <div className="field">
-                        <label>Estimated Finished Qty</label>
-                        <input onChange={this.onChange} type="text" name="estimatedFinishedQty"/>
-                    </div>
-                    <div className="field">
-                        <label>Est Price</label>
-                        <input onChange={this.onChange} type="text" name="estPrice"/>
-                    </div>
-                    <div className="field">
-                        <label>Qty Accepted for Listing</label>
-                        <input onChange={this.onChange} type="text" name="qtyAcceptedForListing"/>
-                    </div>
-                    <div className="field">
-                        <label>Qty Accepted at Delivery</label>
-                        <input onChange={this.onChange} type="text" name="qtyAcceptedAtDelivery"/>
-                    </div>
-                    <div className="field">
-                        <label>Chargebacks</label>
-                        <input onChange={this.onChange} type="text" name="chargebacks"/>
-                    </div>
-                    <div className="field">
-                        <label>Final Price Paid</label>
-                        <input onChange={this.onChange} type="number" name="finalPricePaid"/>
-                    </div>
-                </div>
-            <button type="submit" className="ui button">Add</button>
-            </form>
-        </div>
+            </Fragment>
         )
     }
 }
