801974be74ecd745e4815f3f4b9527572a82f427
Jeff Kwok <jkwok97@gmail.com>
2019-02-14 10:51:27 -0700

    refactored get all items by user id app route in python

--- back-end/server.py
+++ back-end/server.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret'
 
-@app.route("/all_items")
-def all_items():
+@app.route("/all_items/<user_id>")
+def all_items(user_id):
 
-    items = server_func.get_all_offered_items_by_user_id()
+    items = server_func.get_all_offered_items_by_user_id(user_id)
     #json.dumps is used to fix previous serialize error,
     #it converts date and decimal to string
-    dump = json.dumps(items,  indent=0, default=str)
-    return jsonify(dump)
+    # dump = json.dumps(items,  indent=0, default=str)
+    return jsonify(items)
 
 if __name__ == '__main__':
     app.run(debug=True)
