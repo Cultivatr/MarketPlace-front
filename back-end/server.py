862e644f37745a3c28d926c6435dea90420b596e
Jeff Kwok <jkwok97@gmail.com>
2019-02-14 15:19:37 -0700

    started function to add new items in python

--- back-end/server.py
+++ back-end/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret'
 
-@app.route("/all_items/<user_id>")
+@app.route("/all_items/<user_id>", methods=['GET'])
 def all_items(user_id):
 
     items = server_func.get_all_offered_items_by_user_id(user_id)
@@ -17,5 +17,46 @@ def all_items(user_id):
     # dump = json.dumps(items,  indent=0, default=str)
     return jsonify(items)
 
+@app.route("/add_items/<user_id>", methods=['POST', 'GET'])
+def add_items(user_id):
+    data = request.get_json()
+    user_id = data.get('userId')
+    name = data.get('type')
+    qty = data.get('quantity')
+    est_birthday = data.get('birthdate')
+    registration_number = data.get('regNumber')
+    rfid_tag = data.get('rfid')
+    breed = data.get('breed')
+    single_brand = data.get('')
+    starting_date_of_feed = data.get('dateOnFeed')
+    type_of_feed = data.get('typeOfFeed')
+    est_completion_date = data.get('estCompletionData')
+    starting_weight = data.get('estStartingWeight')
+    est_finished_weight = data.get('estFinishedWeight')
+    hanging_weight = data.get('hangingWeight')
+    est_price_to_be_paid = data.get('estFinalPrice')
+
+    date_planted = data.get('datePlanted')
+    seed_type = data.get('seedType')
+    heirloom = data.get('heirloom')
+    gmo = data.get('gmo')
+    fertilizer_type_used = data.get('fertilizerTypeUsed')
+    pesticide_type_used = data.get('pesticideTypeUsed')
+    estimated_qty_planted = data.get('estimatedQtyPlanted')
+    estimated_finished_qty = data.get('estimatedFinishedQty')
+    qty_accepted_for_listing = data.get('qtyAcceptedForListing')
+    qty_accepted_at_delivery = data.get('qtyAcceptedAtDelivery')
+    chargebacks = data.get('chargebacks')
+
+    newItems = server_func.add_offered_item_by_user_id(name, user_id, qty, est_birthday, registration_number,rfid_tag, breed, single_brand,
+        starting_date_of_feed, type_of_feed, est_completion_date, starting_weight, est_finished_weight, hanging_weight,
+        est_price_to_be_paid, date_planted, seed_type, heirloom, gmo, fertilizer_type_used, pesticide_type_used,
+        estimated_qty_planted, estimated_finished_qty, qty_accepted_for_listing, qty_accepted_at_delivery, chargebacks)
+
+    return jsonify(newItems)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
--- back-end/server_func.py
+++ back-end/server_func.py
@@ -9,6 +9,17 @@ def get_all_offered_items_by_user_id(id):
     cur = conn.cursor()
 
     cur.execute(f'SELECT * FROM offered_item where user_id = {id};')
+    # select statement will need to be refactored
+    # sql statement from node file is as follows:
+    # `SELECT OFFERED_ITEM.*, STATUS_TRACKER.*, R_STATUS.STATUS_NAME \
+	# FROM OFFERED_ITEM \
+	# FULL JOIN STATUS_TRACKER \
+	# ON OFFERED_ITEM.ID = STATUS_TRACKER.OFFER_ID \
+	# INNER JOIN R_STATUS \
+	# ON STATUS_TRACKER.STATUS_ID = R_STATUS.ID \
+	# WHERE USER_ID = ${user_id} \
+	# AND END_DATE IS NULL;
+    # above is not for MVP but will be required to keep history of offered items and status
 
     offered_item_array = cur.fetchall()
 
@@ -21,3 +32,26 @@ def get_all_offered_items_by_user_id(id):
     conn.close()
 
     return offered_item_obj_array
+
+def add_offered_item_by_user_id(name, user_id, qty, est_birthday, registration_number,rfid_tag, breed, single_brand,\
+            starting_date_of_feed, type_of_feed, est_completion_date, starting_weight, est_finished_weight, hanging_weight,\
+            est_price_to_be_paid, date_planted, seed_type, heirloom, gmo, fertilizer_type_used, pesticide_type_used,\
+            estimated_qty_planted, estimated_finished_qty, qty_accepted_for_listing, qty_accepted_at_delivery, chargebacks):
+    conn = psycopg2.connect("dbname=cultivatr")
+    cur = conn.cursor()
+
+    cur.execute(f"INSERT INTO offered_item (name (select product_id from r_product), user_id, qty, est_birthday, registration_number,rfid_tag, breed, single_brand,\
+            starting_date_of_feed, type_of_feed, est_completion_date, starting_weight, est_finished_weight, hanging_weight,\
+            est_price_to_be_paid, date_planted, seed_type, heirloom, gmo, fertilizer_type_used, pesticide_type_used,\
+            estimated_qty_planted, estimated_finished_qty, qty_accepted_for_listing, qty_accepted_at_delivery, chargebacks),\
+            VALUES ('{name}','{user_id}','{qty}', '{est_birthday}', '{registration_number}', '{rfid_tag}', '{breed}',\
+            '{single_brand}', '{starting_date_of_feed}', '{type_of_feed}', '{est_completion_date}', '{starting_weight}',\
+            '{est_finished_weight}', '{hanging_weight}', '{est_price_to_be_paid}', '{date_planted}', '{seed_type}', '{heirloom}',\
+            '{gmo}', '{fertilizer_type_used}', '{pesticide_type_used}', '{estimated_qty_planted}', '{estimated_finished_qty}',\
+            '{qty_accepted_for_listing}', '{qty_accepted_at_delivery}', '{chargebacks}');")
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
