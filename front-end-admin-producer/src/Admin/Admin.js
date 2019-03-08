b83ed0e7f11fcfa2945bf8b7ecbeb895c0756a4f
evolveu <lemi.m@outlook.com>
2019-03-08 10:07:58 -0700

    debug admin.js

--- front-end-admin-producer/src/Admin/Admin.js
+++ front-end-admin-producer/src/Admin/Admin.js
@@ -21,56 +21,56 @@ class Admin extends Component {
     this.state = {
       dataToShow: '',
       data: {},
-      items_produce: [], 
+      items_produce: [],
       items_livestock: [],
       itemProduceDetails: {},
       itemLivestockDetails: {},
-      users:[]
-  }
-}
-
-componentDidMount = async () => {
-  try {
-    const response3 = await fetch(`http://localhost:5000/admin/users`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    })
-    const json3 = await response3.json();
-    this.setState({ users: json3 })
-    const response = await fetch(`http://localhost:5000/items_produce`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json'}
-    })
-    const json = await response.json();
-    this.setState({ items_produce: json });
-    const response2 = await fetch(`http://localhost:5000/items_livestock`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json'}
-    })
-    const json2 = await response2.json();
-    this.setState({ items_livestock: json2 });
-    
-  } catch (error) {
-    console.log(error);
-  }
-  await this.createData();
-}
-
-createData = () => {
-  let i;
-  if (this.state.items_produce.items_produce.length > 0 || this.state.items_livestock.items_livestock.length > 0) {
-    for (i = 0; i < this.state.items_produce.items_produce.length; i++) {
-      this.data.push(this.state.items_produce.items_produce[i]);
+      users: []
     }
-    for (i = 0; i < this.state.items_livestock.items_livestock.length; i++) {
-      this.data.push(this.state.items_livestock.items_livestock[i]);
-    }
-  } else {
-    this.setState({data: this.data})
   }
-  this.setState({data: this.data})
-  filterData(this.state.data, this.pending, this.accepted, this.sold, this.delivered, this.notAccepted)
-}
+
+  componentDidMount = async () => {
+    try {
+      const response3 = await fetch(`http://localhost:5000/admin/users`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const json3 = await response3.json();
+      this.setState({ users: json3 })
+      const response = await fetch(`http://localhost:5000/items_produce`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const json = await response.json();
+      this.setState({ items_produce: json });
+      const response2 = await fetch(`http://localhost:5000/items_livestock`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const json2 = await response2.json();
+      this.setState({ items_livestock: json2 });
+
+    } catch (error) {
+      console.log(error);
+    }
+    await this.createData();
+  }
+
+  createData = () => {
+    let i;
+    if (this.state.items_produce.items_produce.length > 0 || this.state.items_livestock.items_livestock.length > 0) {
+      for (i = 0; i < this.state.items_produce.items_produce.length; i++) {
+        this.data.push(this.state.items_produce.items_produce[i]);
+      }
+      for (i = 0; i < this.state.items_livestock.items_livestock.length; i++) {
+        this.data.push(this.state.items_livestock.items_livestock[i]);
+      }
+    } else {
+      this.setState({ data: this.data })
+    }
+    this.setState({ data: this.data })
+    filterData(this.state.data, this.pending, this.accepted, this.sold, this.delivered, this.notAccepted)
+  }
 
   removeOverlay = (event) => {
     document.getElementById("itemProduceOverlay").style.display = "none";
@@ -84,7 +84,7 @@ createData = () => {
         this.produceItems.push(this.state.data[i]);
       } else if (this.state.data[i].breed) {
         this.livestockItems.push(this.state.data[i]);
-      } 
+      }
     }
     if (e.target.id.search("P") === 0) {
       await this.setState({ itemProduceDetails: getItemDetails(e.target.id, this.produceItems) });
@@ -134,82 +134,83 @@ createData = () => {
   }
 
   render() {
+
     let toShow;
     if (this.state.dataToShow === 'toBeAccepted') {
-      toShow = 
-      <div className={Class.container2}>
-        <div className={Class.containerTitle}>
-          <h4>Items To Be Accepted Conditionally</h4>
+      toShow =
+        <div className={Class.container2}>
+          <div className={Class.containerTitle}>
+            <h4>Items To Be Accepted Conditionally</h4>
+          </div>
+          <ContainerDashboard data={this.pending} itemObj={this.getItemObj} />
         </div>
-        <ContainerDashboard data={this.pending} itemObj={this.getItemObj}/>
-      </div>
     }
     else if (this.state.dataToShow === 'acceptedConditional') {
-      toShow = 
-      <div className={Class.container2}>
-        <div className={Class.containerTitle}>
-          <h4>Items Accepted Conditionally</h4>
+      toShow =
+        <div className={Class.container2}>
+          <div className={Class.containerTitle}>
+            <h4>Items Accepted Conditionally</h4>
+          </div>
+          <ContainerDashboard data={this.accepted} itemObj={this.getItemObj} />
         </div>
-        <ContainerDashboard data={this.accepted} itemObj={this.getItemObj}/>
-      </div>
     }
     else if (this.state.dataToShow === 'soldToBeDelivered') {
-      toShow = 
-      <div className={Class.container2}>
-        <div className={Class.containerTitle}>
-          <h4>Items Sold To Be Delivered</h4>
+      toShow =
+        <div className={Class.container2}>
+          <div className={Class.containerTitle}>
+            <h4>Items Sold To Be Delivered</h4>
+          </div>
+          <ContainerDashboard data={this.sold} itemObj={this.getItemObj} />
         </div>
-        <ContainerDashboard data={this.sold} itemObj={this.getItemObj}/>
-      </div>
     }
     else if (this.state.dataToShow === 'delivered') {
-      toShow = 
-      <div className={Class.container2}>
-        <div className={Class.containerTitle}>
-          <h4>Items Delivered</h4>
+      toShow =
+        <div className={Class.container2}>
+          <div className={Class.containerTitle}>
+            <h4>Items Delivered</h4>
+          </div>
+          <ContainerDashboard data={this.delivered} itemObj={this.getItemObj} />
         </div>
-        <ContainerDashboard data={this.delivered} itemObj={this.getItemObj}/>
-      </div>
     }
     else if (this.state.dataToShow === 'notAccepted') {
-      toShow = 
-      <div className={Class.container2}>
-        <div className={Class.containerTitle}>
-          <h4>Items Not Accepted</h4>
+      toShow =
+        <div className={Class.container2}>
+          <div className={Class.containerTitle}>
+            <h4>Items Not Accepted</h4>
+          </div>
+          <ContainerDashboard data={this.notAccepted} itemObj={this.getItemObj} />
         </div>
-        <ContainerDashboard data={this.notAccepted} itemObj={this.getItemObj}/>
-      </div>
     }
     else if (this.state.dataToShow === 'listUsers') {
-      toShow = 
-      <div className={Class.container2}>
-        <div className={Class.containerTitle}>
-          <button className={Class.buttonAddUser} onClick={this.OnClickAddUser}>Add User</button>
+      toShow =
+        <div className={Class.container2}>
+          <div className={Class.containerTitle}>
+            <button className={Class.buttonAddUser} onClick={this.OnClickAddUser}>Add User</button>
+          </div>
+          <UsersComp data={this.state.users} removeOverlay={this.removeOverlay} />
         </div>
-        <UsersComp data={this.state.users} removeOverlay={this.removeOverlay}/>
-      </div>
     }
     else if (this.state.dataToShow === 'addNewProd') {
-      toShow = 
-      <div className={Class.container3}>
-        <div className={Class.containerTitle}>
-          <h4><u>Add New Users</u></h4>
+      toShow =
+        <div className={Class.container3}>
+          <div className={Class.containerTitle}>
+            <h4><u>Add New Users</u></h4>
+          </div>
+          <AddNewProdComp />
         </div>
-        <AddNewProdComp />
-      </div>
     }
     return (
       <div className="App">
         <h1 className={Class.heading}>Welcome Dan!</h1>
         <main>
-          <ItemDetailLiveStockComp itemProduceDetails={this.state.itemProduceDetails} removeOverlay={this.removeOverlay}/>
-          <ItemDetailProduceComp itemLivestockDetails={this.state.itemLivestockDetails} removeOverlay={this.removeOverlay}/>
+          <ItemDetailProduceComp itemProduceDetails={this.state.itemProduceDetails} removeOverlay={this.removeOverlay} />
+          <ItemDetailLiveStockComp itemLivestockDetails={this.state.itemLivestockDetails} removeOverlay={this.removeOverlay} />
           <div className={Class.container}>
             <div className={Class.boxContainer}>
               <div className={Class.leftNav}>
                 <button id="button-accept" className={Class.buttonAdmin} onClick={this.OnClickAccept}>Items To Accept</button>
                 <button id="button-conditional" className={Class.buttonAdmin} onClick={this.OnClickConditional}>Accepted Conditionally</button>
-                <button id="button-sold" className={Class.buttonAdmin}onClick={this.OnClickSold}>Sold To Be Delivered</button>
+                <button id="button-sold" className={Class.buttonAdmin} onClick={this.OnClickSold}>Sold To Be Delivered</button>
                 <button id="button-delivered" className={Class.buttonAdmin} onClick={this.OnClickDelivered}>Delivered</button>
                 <button id="button-notAccepted" className={Class.buttonAdmin} onClick={this.OnClickNotAccepted}>Items Not Accepted</button>
                 <button id="button-listUsers" className={Class.buttonAdmin} onClick={this.OnClickListUsers}>List Users</button>
--- front-end-admin-producer/src/Admin/Components/ItemDetailComp/ItemDetailLiveStockComp.js
+++ front-end-admin-producer/src/Admin/Components/ItemDetailComp/ItemDetailLiveStockComp.js
@@ -5,113 +5,113 @@ import "./itemDetailComp.css"
 class ItemDetailLivestockComp extends Component {
     render() {
         console.log(this.props.itemLivestockDetails)
-        // const { id, type, breed, singleBrand, birthdate, regNumber, rfid, estStartingWeight, hangingWeight, 
-        //     chargebacks, comments, deliveredTo, deliveredDate, dateOnFeed, feedMethod, typeOfPasture,
-        //     typeOfFeed, estCompletionDate, estFinishedWeight, estFinalPrice, quantity, finalPrice, status} = this.props.itemLivestockDetails;
-    return (
-        <div id="itemLivestockOverlay">
-            <div className={Class.itemDetailContainer}>
-                <div className={Class.tableHeader}>
-                    <h4 className="ui horizontal divider header">{"type"} | Item # <i>{"id"}</i></h4>
+        const { id, type, breed, singleBrand, birthdate, regNumber, rfid, estStartingWeight, hangingWeight,
+            chargebacks, comments, deliveredTo, deliveredDate, dateOnFeed, feedMethod, typeOfPasture,
+            typeOfFeed, estCompletionDate, estFinishedWeight, estFinalPrice, quantity, finalPrice, status } = this.props.itemLivestockDetails;
+        return (
+            <div id="itemLivestockOverlay">
+                <div className={Class.itemDetailContainer}>
+                    <div className={Class.tableHeader}>
+                        <h4 className="ui horizontal divider header">{type} | Item # <i>{id}</i></h4>
+                    </div>
+                    <div className={Class.userTable}>
+                        <table className="ui definition table">
+                            <tbody>
+                                <tr>
+                                    <td>Status</td>
+                                    <td>{status}</td>
+                                </tr>
+                                <tr>
+                                    <td className="two wide column">Breed</td>
+                                    <td>{breed}</td>
+                                </tr>
+                                <tr>
+                                    <td>Single Brand</td>
+                                    <td>{singleBrand}</td>
+                                </tr>
+                                <tr>
+                                    <td>Estimated Birthdate</td>
+                                    <td>{birthdate}</td>
+                                </tr>
+                                <tr>
+                                    <td>Registration Number</td>
+                                    <td>{regNumber}</td>
+                                </tr>
+                                <tr>
+                                    <td>RFID Tag</td>
+                                    <td>{rfid}</td>
+                                </tr>
+                                <tr>
+                                    <td>Date On Feed</td>
+                                    <td>{dateOnFeed}</td>
+                                </tr>
+                                <tr>
+                                    <td>Feed Method</td>
+                                    <td>{feedMethod}</td>
+                                </tr>
+                                <tr>
+                                    <td>Type Of Pasture</td>
+                                    <td>{typeOfPasture}</td>
+                                </tr>
+                                <tr>
+                                    <td>Type Of Feed</td>
+                                    <td>{typeOfFeed}</td>
+                                </tr>
+                                <tr>
+                                    <td>Est Starting Weight</td>
+                                    <td>{estStartingWeight}</td>
+                                </tr>
+                                <tr>
+                                    <td>Quantity</td>
+                                    <td>{quantity}</td>
+                                </tr>
+                                <tr>
+                                    <td>Est Completion Date</td>
+                                    <td>{estCompletionDate}</td>
+                                </tr>
+                                <tr>
+                                    <td>Est Finished Weight</td>
+                                    <td>{estFinishedWeight}</td>
+                                </tr>
+                                <tr>
+                                    <td>Est Final Price</td>
+                                    <td>{estFinalPrice}</td>
+                                </tr>
+                                <tr>
+                                    <td>Hanging Weight</td>
+                                    <td>{hangingWeight}</td>
+                                </tr>
+                                <tr>
+                                    <td>Final Price</td>
+                                    <td>{finalPrice}</td>
+                                </tr>
+                                <tr>
+                                    <td>Delivered Date</td>
+                                    <td>{deliveredDate}</td>
+                                </tr>
+                                <tr>
+                                    <td>Delivered To</td>
+                                    <td>{deliveredTo}</td>
+                                </tr>
+                                <tr>
+                                    <td>Charge Backs</td>
+                                    <td>{chargebacks}</td>
+                                </tr>
+                                <tr>
+                                    <td>Comments</td>
+                                    <td>{comments}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className={Class.itemButtonsContainer}>
+                        <button className={Class.itemButtonsModify} onClick={this.props.removeOverlay}>Modify</button>
+                        <button className={Class.itemButtonsCancel} onClick={this.props.removeOverlay}>Cancel</button>
+                    </div>
                 </div>
-                <div className={Class.userTable}>
-                    <table className="ui definition table">
-                    <tbody>
-                        <tr>
-                            <td>Status</td>
-                            <td>{"status"}</td>
-                        </tr>
-                        <tr>
-                            <td className="two wide column">Breed</td>
-                            <td>{"breed"}</td>
-                        </tr>
-                        <tr>
-                            <td>Single Brand</td>
-                            <td>{"singleBrand"}</td>
-                        </tr>
-                        <tr>
-                            <td>Estimated Birthdate</td>
-                            <td>{"birthdate"}</td>
-                        </tr>
-                        <tr>
-                            <td>Registration Number</td>
-                            <td>{"regNumber"}</td>
-                        </tr>
-                        <tr>
-                            <td>RFID Tag</td>
-                            <td>{"rfid"}</td>
-                        </tr>
-                        <tr>
-                            <td>Date On Feed</td>
-                            <td>{"dateOnFeed"}</td>
-                        </tr>
-                        <tr>
-                            <td>Feed Method</td>
-                            <td>{"feedMethod"}</td>
-                        </tr>
-                        <tr>
-                            <td>Type Of Pasture</td>
-                            <td>{"typeOfPasture"}</td>
-                        </tr>
-                        <tr>
-                            <td>Type Of Feed</td>
-                            <td>{"typeOfFeed"}</td>
-                        </tr>
-                        <tr>
-                            <td>Est Starting Weight</td>
-                            <td>{"estStartingWeight"}</td>
-                        </tr>
-                        <tr>
-                            <td>Quantity</td>
-                            <td>{"quantity"}</td>
-                        </tr>
-                        <tr>
-                            <td>Est Completion Date</td>
-                            <td>{"estCompletionDate"}</td>
-                        </tr>
-                        <tr>
-                            <td>Est Finished Weight</td>
-                            <td>{"estFinishedWeight"}</td>
-                        </tr>
-                        <tr>
-                            <td>Est Final Price</td>
-                            <td>{"estFinalPrice"}</td>
-                        </tr>
-                        <tr>
-                            <td>Hanging Weight</td>
-                            <td>{"hangingWeight"}</td>
-                        </tr>
-                        <tr>
-                            <td>Final Price</td>
-                            <td>{"finalPrice"}</td>
-                        </tr>
-                        <tr>
-                            <td>Delivered Date</td>
-                            <td>{"deliveredDate"}</td>
-                        </tr>
-                        <tr>
-                            <td>Delivered To</td>
-                            <td>{"deliveredTo"}</td>
-                        </tr>
-                        <tr>
-                            <td>Charge Backs</td>
-                            <td>{"chargebacks"}</td>
-                        </tr>
-                        <tr>
-                            <td>Comments</td>
-                            <td>{"comments"}</td>
-                        </tr>
-                    </tbody>
-                    </table>
-                </div>
-                <div className={Class.itemButtonsContainer}>
-                    <button className={Class.itemButtonsModify} onClick={this.props.removeOverlay}>Modify</button>
-                    <button className={Class.itemButtonsCancel} onClick={this.props.removeOverlay}>Cancel</button>
-                </div> 
             </div>
-        </div> 
-    )
-}
+        )
+    }
 }
 
 export default ItemDetailLivestockComp;
