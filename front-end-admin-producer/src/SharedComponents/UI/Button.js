5ae45719b472d33b754d57c2eb9452ab6bc3deaa
Ana <anakonda.88@gmail.com>
2019-03-11 15:22:06 -0600

    animating add button try one

--- front-end-admin-producer/src/SharedComponents/UI/Button.js
+++ front-end-admin-producer/src/SharedComponents/UI/Button.js
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from './Button.module.css'
 
 const Button = (props) => (
-   <button 
+   <button
       type="submit"
       onClick={props.clicked}
       className={styles.button}>
       {props.children}
    </button>
 )
+
 
 export default Button;
