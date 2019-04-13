278e15a91bcb746d7938afea321539fd6d50ad6f
Seunjules <seun.mejule@gmail.com>
2019-04-13 14:03:34 -0600

    Dev Ops

--- back-end/app.py
+++ back-end/app.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'dev_key',
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db=SQLAlchemy(app)
